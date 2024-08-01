7434a9798ceb0ab77ed3afd60b1c8136ec6f58ee
Philip Karanja <kpk.my.mailings@gmail.com>
2024-07-31 20:26:13 -0500

    Update _app.js

--- starter-template/src/pages/_app.js
+++ starter-template/src/pages/_app.js
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
