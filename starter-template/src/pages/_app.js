f17d91ad9f28cfefcfa74a740f192ec2d036dad1
Philip Karanja <kpk.my.mailings@gmail.com>
2024-08-01 16:44:48 -0500

    BoilerPlate Layout
    Made a base layout needs some formatting on the search bar

--- starter-template/src/pages/_app.js
+++ starter-template/src/pages/_app.js
@@ -1,6 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '/styles/globals.css';
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
--- starter-template/src/pages/_document.js
+++ starter-template/src/pages/_document.js
@@ -1,9 +1,30 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
+        <script
+          src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+          integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+          integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGaU5Tw6Wc4GIy76P1Lt8JH2xr"
+          crossOrigin="anonymous"
+        ></script>
+        <script
+          src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+          integrity="sha384-ChfqqxuZUCnJSK3+djyKjLhcHYiirfHf+L6M5RQjtiYYfRRaIdRdftMUI8dCqldw"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
