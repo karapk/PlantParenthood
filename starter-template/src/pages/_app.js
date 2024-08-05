34ea560c8731e2368bdebe5c7bab749927026de8
Philip Karanja <kpk.my.mailings@gmail.com>
2024-08-05 04:26:40 -0500

    WIP

--- starter-template/src/pages/_app.js
+++ starter-template/src/pages/_app.js
@@ -1,9 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Layout from '../components/Layout';
 import '../styles/globals.css';
 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
---
+++ starter-template/src/pages/aboutUs.js
@@ -0,0 +1,10 @@
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the about us page.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
