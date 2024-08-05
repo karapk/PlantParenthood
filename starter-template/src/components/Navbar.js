91da57032118ba736d1fa98fb7b9eb9f11654f09
Philip Karanja <kpk.my.mailings@gmail.com>
2024-08-05 17:03:36 -0500

    WIP

---
+++ starter-template/src/components/Navbar.js
@@ -0,0 +1,78 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark custom-navbar">
+        <a className="navbar-brand" href="#">
+          PlantParentHood
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarsExampleDefault"
+          aria-controls="navbarsExampleDefault"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/aboutUs">
+                About Us
+              </a>
+            </li>
+            {/* <li className="nav-item">
+              <a className="nav-link disabled" href="#">
+                Disabled
+              </a>
+            </li> */}
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="dropdown01"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Plant Type
+              </a>
+              <div className="dropdown-menu" aria-labelledby="dropdown01">
+                <a className="dropdown-item" href="#">
+                  Action
+                </a>
+                <a className="dropdown-item" href="#">
+                  Another action
+                </a>
+                <a className="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </div>
+            </li>
+          </ul>
+          <form className="form-inline ml-auto">
+            <input
+              className="form-control mr-sm-2"
+              type="text"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+  );
+};
+
+export default Navbar;
