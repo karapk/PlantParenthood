08fb39cc344e62a8feb5e8e281a04955b617e6f7
Khoi <lethanhkhoi01@gmail.com>
2024-08-09 17:03:44 -0500

    Add the Footer to the main page

--- starter-template/src/pages/index.js
+++ starter-template/src/pages/index.js
@@ -1,9 +1,12 @@
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div>
-      <Header />
-    </div>
+      <div className="page-container">
+          <Header/>
+          <Footer/>
+
+      </div>
   );
 }
