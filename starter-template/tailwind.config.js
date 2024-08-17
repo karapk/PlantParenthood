c856c4838f71ce9e11788933bd55fc9daf206a06
Philip Karanja <kpk.my.mailings@gmail.com>
2024-08-17 11:11:50 -0500

    Update tailwind.config.js

--- starter-template/tailwind.config.js
+++ starter-template/tailwind.config.js
@@ -5,8 +5,8 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
  
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    // // Or if using `src` directory:
+    // "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
