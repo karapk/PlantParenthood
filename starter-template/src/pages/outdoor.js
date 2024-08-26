a9421d1372cff41109befdb9b640acb57598d8f4
Focusedplumes <152942773+Focusedplumes@users.noreply.github.com>
2024-08-25 22:49:02 -0500

    Cards!
    Cards made more styling to come.

--- starter-template/src/pages/outdoor.js
+++ starter-template/src/pages/outdoor.js
@@ -1,5 +1,6 @@
 import { prisma } from "../server/prisma";
 
+
 export async function getServerSideProps() {
   const outsidePlants = await prisma.plants.findMany({where: {
     isOutdoor: true,
@@ -7,6 +8,7 @@ export async function getServerSideProps() {
 }); 
   return {
     props: { outsidePlants: outsidePlants || [] }, 
+
   };
 }
 export default function OutdoorPage({ outsidePlants }) {
@@ -16,19 +18,30 @@ export default function OutdoorPage({ outsidePlants }) {
   }
   
   return (
-    <>
+  <>
+  <div>
+    <h1 className="text-3xl font-bold underline">Outdoor Plants</h1>
+    <p>
+    Outdoor plants are a vital part of any garden or landscape, bringing life, color, and natural beauty to outdoor spaces. They range from vibrant flowers and lush shrubs to towering trees and delicate ground covers, each contributing to the ecosystem while enhancing the visual appeal of your environment. Outdoor plants not only improve air quality and provide habitats for wildlife but also offer a sense of tranquility and connection to nature. Whether you're cultivating a small balcony garden or a sprawling backyard, choosing the right outdoor plants can transform your space into a personal oasis.</p>
+  </div>
+
+  <div>
     {outsidePlants.map((item) => (
       item.isOutdoor && (  // Only render if the plant is outdoor
-        <div key={item.isOutdoor} className='item'>
-          <div className="item-names">{item.name}</div>
-          <div className="item-names">{item.genus}</div>
-          <div className='item-description'>{item.description}</div>
-          {/* Uncomment and use the following line if you have an image URL */}
-          {/* <img src={item.imageUrl} alt={item.itemName} className='item-img' /> */}
+        
+        <div key={item.isOutdoor} className="card" style={{width: '18rem'}}>
+          <img src={item.imgURL} className="card-img-top" alt={item.name} />
+          <div className='card-body'>
+            <h5 className="card-title">{item.name}</h5>
+            <h6 className='card-subtitle mb-2 text-muted'>{item.genus}</h6>
+            <p className="card-text">{item.description}</p>  
         </div>
+        </div>
+        
       )
     ))}
+  </div>  
   </>
 
     
-  )}
+  );}
