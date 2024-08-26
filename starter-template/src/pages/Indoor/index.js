46035725830e2a929a803fdfa7885828e985a4d9
Philip Karanja <kpk.my.mailings@gmail.com>
2024-08-26 01:03:15 -0500

    WIP

--- starter-template/src/pages/Indoor/index.js
+++ starter-template/src/pages/Indoor/index.js
@@ -1,7 +1,20 @@
 export default function Indoor() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Indoor</h1>
+  <div class="card" style={{width: "18rem"}}>
+    <img class="card-img-top" alt="Card image cap"/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Cras justo odio</li>
+      <li class="list-group-item">Dapibus ac facilisis in</li>
+      <li class="list-group-item">Vestibulum at eros</li>
+    </ul>
+    <div class="card-body">
+      <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a>
+    </div>
+  </div>
   );
 }
